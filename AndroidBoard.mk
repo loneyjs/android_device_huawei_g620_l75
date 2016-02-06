afe4679090603b1146c04c28e5bf51abc14661d6
Crixec <79236107@qq.com>
2016-02-06 16:46:42 +0800

    use prebuilt kernel

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -2,15 +2,16 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-#TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
-#INSTALLED_KERNEL_TARGET := $(TARGET_PREBUILT_KERNEL)
+# Use prebuilt kernel
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
+INSTALLED_KERNEL_TARGET := $(TARGET_PREBUILT_KERNEL)
 
-#file := $(INSTALLED_KERNEL_TARGET)
-#ALL_PREBUILT += $(file)
-#$(file) : $(TARGET_PREBUILT_KERNEL) | $(ACP)
-#	$(transform-prebuilt-to-target)
+file := $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(file)
+$(file) : $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
 
-#ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
 -include vendor/huawei/g620_l75/AndroidBoardVendor.mk
